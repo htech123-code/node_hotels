238dc3993ae041ad2db9ee7660bb0e442731bdbd
htech123-code <htechlearning1@gmail.com>
2024-08-29 00:11:34 -0700

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const db=require("./db")
+require("dotenv").config()
 
 const bodyParser=require("body-parser")
 app.use(bodyParser.json())
@@ -33,6 +34,7 @@ app.use("/person",personRoutes)
 app.use("/menu",menuRoutes)
 
 
+const PORT=process.env.PORT || 3000
 
 
-app.listen(3000,()=>console.log(`server started at 3000`))
+app.listen(PORT,()=>console.log(`server started at 3000`))
